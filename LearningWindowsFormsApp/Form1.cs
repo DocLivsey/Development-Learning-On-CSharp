264c6a9ba636d6528d5592962015e8276389af49
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-02-21 01:10:30 +0300

    restored class practice

---
+++ LearningWindowsFormsApp/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LearningWindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        protected List<User> users;
+        public Form1()
+        {
+            users = new List<User>();
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void OK_Click(object sender, EventArgs e)
+        {
+            string text = "\n";
+            text += textBox1.Text + "\n";
+            text += comboBox1.Text + "\n";
+            if (radioButton1.Checked)
+                text += "male\n";
+            else if (radioButton2.Checked)
+                text += "female\n";
+            else if (radioButton3.Checked)
+                text += "fool\n";
+            text += textBox2.Text + "\n";
+            richTextBox1.AppendText(text);
+            User user = new User(textBox1.Text, comboBox1.Text);
+            users.Add(user);
+            MessageBox.Show("button1 was clicked");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ LearningWindowsFormsApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LearningWindowsFormsApp
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+
+    public class User
+    {
+        protected string Name { set; get; }
+        protected string Address { set; get; }
+        public User(string name, string address)
+        {
+            this.Name = name;
+            this.Address = address;
+        }
+    }
+}
